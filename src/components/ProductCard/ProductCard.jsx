324f1726e72372175fae1d1d7e40b33afaf71757
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-20 15:40:34 +0530

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,1 +1,13 @@
+function ProductCard({ product }) {
+  return (
+    <div>
+      <img src={product.image} alt={product.name} />
 
+      <h3>{product.name}</h3>
+
+      <p>${product.price}</p>
+    </div>
+  )
+}
+
+export default ProductCard
