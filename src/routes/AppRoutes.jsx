e63218c97961dec9faeb5f587568c893b5c62323
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-20 15:38:31 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import Cart from '../pages/Cart'
+import Login from '../pages/Login'
+import NotFound from '../pages/NotFound'
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
