da5a7889c575f16f3015b879275ded0e8ec16479
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-20 15:41:16 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,1 +1,28 @@
+import { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard/ProductCard'
+import { getProducts } from '../services/productService'
 
+function Products() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const fetchProducts = async () => {
+    const data = await getProducts()
+    setProducts(data)
+  }
+
+  return (
+    <div>
+      <h1>Products</h1>
+
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default Products
