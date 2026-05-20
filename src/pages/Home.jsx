af0c47d9000a395877948dee0094f13f63f4acb0
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-20 15:40:50 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,16 @@
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Welcome to Dolls World</h1>
+
+      <Footer />
+    </>
+  )
+}
+
+export default Home
